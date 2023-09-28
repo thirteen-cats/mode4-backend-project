f50ffe256d7a78c25afc4b6e84be9f0539bf09ac
Mariiag <M.gribacheva@gmail.com>
2023-09-27 22:03:08 -0700

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,17 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
+
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
+//Initialize the Express application:
 const app = express();
-
+//Connect the morgan middleware for logging information about requests and responses:
 app.use(morgan('dev'));
-
+//Add the cookie-parser middleware for parsing cookies and the express.json middleware for parsing JSON bodies of requests with Content-Type of "application/json".
 app.use(cookieParser());
 app.use(express.json());
 
@@ -42,11 +45,45 @@ if (!isProduction) {
 
 const routes = require('./routes');
 
-
 app.use(routes); // Connect all the routes
 
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 
 
-  //-----------------------------------//
-  module.exports = app;
+//--------Do not change the export statement below:-----//
+module.exports = app;
